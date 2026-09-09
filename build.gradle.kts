30a2f734b8f6ec630b1c3464f2d8b2cacd7c3c70
Korol Igor <korol.igor.alexandrovi4@gmail.com>
2026-09-09 23:37:47 +0500

    инициализирован spring boot с тестовым rest контроллером

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,21 @@
 plugins {
     id("java")
+    // Плагин Spring Boot (управляет сборкой, запуском bootRun и созданием fat-jar)
+    id("org.springframework.boot") version "3.3.3"
+
+    // Плагин управления зависимостями (автоматически определяет версии для starter-библиотек)
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Строго фиксируем 21 (или 17)
+    }
+}
+
+// Указываем плагину точное имя нашего главного класса
+springBoot {
+    mainClass.set("org.example.Main")
 }
 
 group = "org.example"
@@ -13,8 +29,14 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Стартер для создания Web-приложений (REST API, MVC, встроенный Tomcat)
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Стартер для тестирования (JUnit 5, Mockito, AssertJ)
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
+tasks.withType<Test> {
+    // Обязательно для работы тестов на JUnit 5
     useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,7 @@
+plugins {
+    // Вставляем плагин именно сюда!
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "simple_entry_back"
 include("contractor-module")
